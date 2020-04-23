5220a6e857d955134cef3c0f5fc46c6734f0c309
Santhoshraj A Antony <santhosh.antony@gmail.com>
2020-04-23 10:28:02 +0530

    making code ready for heroku deploymnet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const htmlPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -82,6 +83,6 @@ app.get('*',(req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log ('server starting at port 3000')
+app.listen(port, () => {
+    console.log ('server starting at port: ' + port)
 })
